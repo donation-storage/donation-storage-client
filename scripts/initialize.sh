34b1e0a35af32919bec013eaefe015e05586554e
GWANGUIAN <bbaa3218@gmail.com>
2022-08-15 22:12:47 +0900

    chore: test for docker-compose

--- scripts/initialize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/donation-storage-client
-yarn install
-yarn global add pm2@latest
-sudo apt-get update
-sudo apt-get install authbind
-sudo touch /etc/authbind/byport/80
-sudo chown ubuntu /etc/authbind/byport/80
-sudo chmod 755 /etc/authbind/byport/80
--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/donation-storage-client
-
-export NEXT_PUBLIC_USE_API_MOCKING=$(aws ssm get-parameters --region ap-northeast-2 --names NEXT_PUBLIC_USE_API_MOCKING --query Parameters[0].Value | sed 's/"//g')
-export NEXT_PUBLIC_SERVER_API=$(aws ssm get-parameters --region ap-northeast-2 --names NEXT_PUBLIC_SERVER_API --query Parameters[0].Value | sed 's/"//g')
-export NEXT_PUBLIC_CLINET_DOMAIN=$(aws ssm get-parameters --region ap-northeast-2 --names NEXT_PUBLIC_CLINET_DOMAIN --query Parameters[0].Value | sed 's/"//g')
-export NEXT_PUBLIC_CLINET_ORIGIN=$(aws ssm get-parameters --region ap-northeast-2 --names NEXT_PUBLIC_CLINET_ORIGIN --query Parameters[0].Value | sed 's/"//g')
-
-authbind --deep pm2 start yarn --name "nextjs" -- run serve -- --port=80
