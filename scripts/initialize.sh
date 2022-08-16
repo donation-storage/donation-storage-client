52866c640a3a4e0e3972a68bc1ad1abe9fa3a256
GWANGUIAN <bbaa3218@gmail.com>
2022-08-16 21:56:06 +0900

    chore: modify script files for codeDeploy

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,4 +1,9 @@
 cd /home/ubuntu/donation-storage-client
+
+docker kill $(docker ps -q)
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images)
+
 docker system prune --all --force
 
 docker pull public.ecr.aws/g6t4g5a6/donation-storage-client:client
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,5 @@
 cd /home/ubuntu/donation-storage-client
 
-docker kill $(docker ps -q) 2> /dev/null || true
+docker kill $(docker ps -q)
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images)
